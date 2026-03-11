88a7d3ae0048552363c0ff5d9b53ac649fd37e99
Obed-Ojingwa <obedojingwa@gmail.com>
2026-03-11 17:11:11 +0100

    New error on graddle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
 
 }
 
@@ -72,6 +74,9 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.57.1")
 
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+    implementation("androidx.compose.material:material-icons-core:1.6.0")
+    ksp("androidx.hilt:hilt-compiler:1.2.0")
 
 
     implementation(libs.androidx.core.ktx)
