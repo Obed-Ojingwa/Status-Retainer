56bf3038e40d1340ee3cba9d3da6acd1f367c6ec
Obed-Ojingwa <obedojingwa@gmail.com>
2026-03-12 14:49:22 +0100

    fixed previous gradle wahala

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
-    id("com.google.devtools.ksp")
-    id("com.google.dagger.hilt.android")
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
