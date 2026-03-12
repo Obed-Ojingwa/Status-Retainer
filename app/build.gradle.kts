6898013349cb89ce31234218d5fde8dd88754f31
Obed-Ojingwa <obedojingwa@gmail.com>
2026-03-12 14:04:05 +0100

    fixing the  error on graddle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,23 +1,18 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
-    id("com.google.dagger.hilt.android")
     id("com.google.devtools.ksp")
-
+    id("com.google.dagger.hilt.android")
 }
 
 android {
     namespace = "com.nerdpace.statussaver"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.nerdpace.statussaver"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -43,50 +38,46 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.hilt.work)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.navigation.compose)
-    val room_version = "2.8.4"
+    val room_version = "2.7.1"
     val work_version = "2.11.1"
 
+    // Room
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    ksp("androidx.room:room-compiler:$room_version")
 
-    implementation("androidx.room:room-runtime:${room_version}")
+    // WorkManager
+    implementation("androidx.work:work-runtime-ktx:$work_version")
+    implementation("androidx.work:work-gcm:$work_version")
+    implementation("androidx.work:work-multiprocess:$work_version")
 
-
-    implementation("androidx.room:room-ktx:${room_version}")
-
-
-
-    implementation("androidx.work:work-runtime-ktx:${work_version}")
-
-    implementation("androidx.work:work-gcm:${work_version}")
-
-    implementation("androidx.work:work-multiprocess:${work_version}")
-
-
+    // Coil
     implementation("io.coil-kt.coil3:coil-compose:3.4.0")
     implementation("io.coil-kt.coil3:coil-network-okhttp:3.4.0")
 
-
-
-    implementation("androidx.hilt:hilt-navigation-fragment:1.0.0")
-    implementation("androidx.hilt:hilt-work:1.0.0")
+    // Hilt
     implementation("com.google.dagger:hilt-android:2.57.1")
-
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
-
-    implementation("androidx.compose.material:material-icons-core:1.6.0")
+    ksp("com.google.dagger:hilt-android-compiler:2.57.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation("androidx.hilt:hilt-navigation-fragment:1.2.0")
+    implementation("androidx.hilt:hilt-work:1.2.0")
     ksp("androidx.hilt:hilt-compiler:1.2.0")
 
+    // Icons
+    implementation("androidx.compose.material:material-icons-core:1.7.8")
 
+    // AndroidX
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -95,3 +86,102 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
+
+
+//plugins {
+//    alias(libs.plugins.android.application)
+//    alias(libs.plugins.kotlin.compose)
+//    id("com.google.dagger.hilt.android")
+//    id("com.google.devtools.ksp")
+//
+//}
+//
+//android {
+//    namespace = "com.nerdpace.statussaver"
+//    compileSdk {
+//        version = release(36) {
+//            minorApiLevel = 1
+//        }
+//    }
+//
+//    defaultConfig {
+//        applicationId = "com.nerdpace.statussaver"
+//        minSdk = 24
+//        targetSdk = 36
+//        versionCode = 1
+//        versionName = "1.0"
+//
+//        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+//    }
+//
+//    buildTypes {
+//        release {
+//            isMinifyEnabled = false
+//            proguardFiles(
+//                getDefaultProguardFile("proguard-android-optimize.txt"),
+//                "proguard-rules.pro"
+//            )
+//        }
+//    }
+//    compileOptions {
+//        sourceCompatibility = JavaVersion.VERSION_11
+//        targetCompatibility = JavaVersion.VERSION_11
+//    }
+//    buildFeatures {
+//        compose = true
+//    }
+//}
+//
+//dependencies {
+//    implementation(libs.androidx.hilt.work)
+//    implementation(libs.androidx.material3)
+//    implementation(libs.androidx.navigation.compose)
+//    val room_version = "2.8.4"
+//    val work_version = "2.11.1"
+//
+//
+//    implementation("androidx.room:room-runtime:${room_version}")
+//
+//
+//    implementation("androidx.room:room-ktx:${room_version}")
+//
+//
+//
+//    implementation("androidx.work:work-runtime-ktx:${work_version}")
+//
+//    implementation("androidx.work:work-gcm:${work_version}")
+//
+//    implementation("androidx.work:work-multiprocess:${work_version}")
+//
+//
+//    implementation("io.coil-kt.coil3:coil-compose:3.4.0")
+//    implementation("io.coil-kt.coil3:coil-network-okhttp:3.4.0")
+//
+//
+//
+//    implementation("androidx.hilt:hilt-navigation-fragment:1.0.0")
+//    implementation("androidx.hilt:hilt-work:1.0.0")
+//    implementation("com.google.dagger:hilt-android:2.57.1")
+//
+//    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+//
+//    implementation("androidx.compose.material:material-icons-core:1.6.0")
+//    ksp("androidx.hilt:hilt-compiler:1.2.0")
+//
+//
+//    implementation(libs.androidx.core.ktx)
+//    implementation(libs.androidx.lifecycle.runtime.ktx)
+//    implementation(libs.androidx.activity.compose)
+//    implementation(platform(libs.androidx.compose.bom))
+//    implementation(libs.androidx.compose.ui)
+//    implementation(libs.androidx.compose.ui.graphics)
+//    implementation(libs.androidx.compose.ui.tooling.preview)
+//    implementation(libs.androidx.compose.material3)
+//    testImplementation(libs.junit)
+//    androidTestImplementation(libs.androidx.junit)
+//    androidTestImplementation(libs.androidx.espresso.core)
+//    androidTestImplementation(platform(libs.androidx.compose.bom))
+//    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+//    debugImplementation(libs.androidx.compose.ui.tooling)
+//    debugImplementation(libs.androidx.compose.ui.test.manifest)
+//}
